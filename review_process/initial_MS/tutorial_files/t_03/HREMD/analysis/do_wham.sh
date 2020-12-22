e8eb20572d21c37feb9caab136dab6b7b481c6ac
Niels <niels.hansen@itt.uni-stuttgart.de>
2020-12-22 14:34:49 +0100

    clean up directories

--- review_process/initial_MS/tutorial_files/t_03/HREMD/analysis/do_wham.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#perform wham and move the curve such that the minimum is at 0
-./wham.py ../HREMD.imd ../disres.dat
-awk 'NR==1 { min=$2 } 
-     FNR==NR { if ($2<=min) min=$2 ; next}
-     { $2=($2-min) ; print}' wham_FEC_200bins_5000iter.dat wham_FEC_200bins_5000iter.dat > wham_FEC_200bins_5000iter_min0.dat
-
